6f3e9400713cfea7b3fae8069b26bdaf0151cdbb
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-28 19:15:57 +0100

    Add site config
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

---
+++ content/js/sitename.js
@@ -0,0 +1,20 @@
+/* MarkdownBlog
+ * Write blogs in markdown and generate static html files that are
+ * ready to be served by a web server and are hackable.
+ * Github: https://www.github.com/0x4248/MarkdownBlog
+ * Licence: GNU General Public License v3.0
+ * By: 0x4248 
+ * 
+ * content/js/sitename.js
+ * Reads the config file and sets the site name in the title
+*/
+
+function setSiteName() {
+    fetch('/config/siteconf.json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('sitename').innerHTML = data.sitename;
+    });
+}
+
+setSiteName();
